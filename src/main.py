896b14d077a5fc2e5ce4eb6b9958f0f94372f482
venkatkrishna <venkatkrishna@moe>
2025-02-10 19:56:46 +0100

    Reset to 8f449d69ca00b0e785e6b3de2733e4d6065ad572 and made changes for LLM Round 1

--- src/main.py
+++ src/main.py
@@ -4,21 +4,22 @@ from typing import List
 from loguru import logger
 
 from src.code_embedding import CodeEmbedder
-from src.script_content_reader import ScriptContentReaderInterface
-from src.script_metadata_extractor import ScriptMetadataExtractorInterface
+from src.script_content_reader import ScriptContentReader
+from src.script_metadata_extractor import ScriptMetadataExtractor
 
 
-def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--readme-paths", nargs="+", type=str, help="Paths to Readme files", default=["README.md"]
-    )
-    return parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--readme-paths", nargs="+", type=str, help="Paths to Readme files", default="README.md"
+)
 
 
-def main(readme_paths: List[str]) -> None:
-    script_metadata_extractor: ScriptMetadataExtractorInterface = ScriptMetadataExtractorInterface()
-    script_content_reader: ScriptContentReaderInterface = ScriptContentReaderInterface()
+def main() -> None:
+    args = parser.parse_args()
+    readme_paths = args.readme_paths if isinstance(args.readme_paths, list) else [args.readme_paths]
+
+    script_metadata_extractor = ScriptMetadataExtractor()
+    script_content_reader = ScriptContentReader()
     code_embedder = CodeEmbedder(
         readme_paths=readme_paths,
         script_metadata_extractor=script_metadata_extractor,
@@ -29,5 +30,4 @@ def main(readme_paths: List[str]) -> None:
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    main(args.readme_paths)
+    main()
