2baaea942023cb4dcff1b4ef0f5b26af9139821c
venkatkrishna <venkatkrishna@moe>
2025-02-10 20:10:00 +0100

    Reset to 8f449d69ca00b0e785e6b3de2733e4d6065ad572 and made changes for LLM Round 1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import argparse
-from typing import List
 
 from loguru import logger
 
@@ -12,22 +11,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--readme-paths", nargs="+", type=str, help="Paths to Readme files", default="README.md"
 )
+args = parser.parse_args()
 
-
-def main() -> None:
-    args = parser.parse_args()
-    readme_paths = args.readme_paths if isinstance(args.readme_paths, list) else [args.readme_paths]
-
+if __name__ == "__main__":
     script_metadata_extractor = ScriptMetadataExtractor()
     script_content_reader = ScriptContentReader()
     code_embedder = CodeEmbedder(
-        readme_paths=readme_paths,
+        readme_paths=args.readme_paths,
         script_metadata_extractor=script_metadata_extractor,
         script_content_reader=script_content_reader,
     )
     code_embedder()
     logger.info("Code Embedder finished successfully.")
-
-
-if __name__ == "__main__":
-    main()
